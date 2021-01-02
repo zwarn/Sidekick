57e0fc65eaf83669461ad9b2af0fd65483920167
zwarn <michael.budahn@gmail.com>
2021-01-02 22:09:34 +0100

    basic movement and animation

---
+++ Assets/Scripts/Input.cs
@@ -0,0 +1,234 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Input : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Input()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Input"",
+    ""maps"": [
+        {
+            ""name"": ""InGame"",
+            ""id"": ""30517ef4-b640-4e99-b297-ed3a0008a6a3"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""cb28add0-7d5d-4b9b-9816-6591025e4b59"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""a0fbc4c5-8d1c-400a-a677-ef9ffdb49aae"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Strike"",
+                    ""type"": ""Button"",
+                    ""id"": ""10736443-629f-4d83-830f-ea65af4ffb96"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""3f6514da-22e4-4572-bb12-540876b79ee5"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""4154ee24-e809-4cc1-b91d-13bb2897de8f"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""4424110e-2a68-4715-b68f-ca1432544b70"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""7fd9949e-e5cf-4287-a3b6-4cf9432f278a"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""34a5f313-50c2-4d06-bf9f-798a8e2f797c"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""3919cbad-386e-4f24-912f-4d020b3c7c7c"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""a3e88516-bd7b-48cd-9eb1-ebac68e6e35c"",
+                    ""path"": ""<Keyboard>/ctrl"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Strike"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // InGame
+        m_InGame = asset.FindActionMap("InGame", throwIfNotFound: true);
+        m_InGame_Movement = m_InGame.FindAction("Movement", throwIfNotFound: true);
+        m_InGame_Jump = m_InGame.FindAction("Jump", throwIfNotFound: true);
+        m_InGame_Strike = m_InGame.FindAction("Strike", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // InGame
+    private readonly InputActionMap m_InGame;
+    private IInGameActions m_InGameActionsCallbackInterface;
+    private readonly InputAction m_InGame_Movement;
+    private readonly InputAction m_InGame_Jump;
+    private readonly InputAction m_InGame_Strike;
+    public struct InGameActions
+    {
+        private @Input m_Wrapper;
+        public InGameActions(@Input wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_InGame_Movement;
+        public InputAction @Jump => m_Wrapper.m_InGame_Jump;
+        public InputAction @Strike => m_Wrapper.m_InGame_Strike;
+        public InputActionMap Get() { return m_Wrapper.m_InGame; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(InGameActions set) { return set.Get(); }
+        public void SetCallbacks(IInGameActions instance)
+        {
+            if (m_Wrapper.m_InGameActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_InGameActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_InGameActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_InGameActionsCallbackInterface.OnMovement;
+                @Jump.started -= m_Wrapper.m_InGameActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_InGameActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_InGameActionsCallbackInterface.OnJump;
+                @Strike.started -= m_Wrapper.m_InGameActionsCallbackInterface.OnStrike;
+                @Strike.performed -= m_Wrapper.m_InGameActionsCallbackInterface.OnStrike;
+                @Strike.canceled -= m_Wrapper.m_InGameActionsCallbackInterface.OnStrike;
+            }
+            m_Wrapper.m_InGameActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Strike.started += instance.OnStrike;
+                @Strike.performed += instance.OnStrike;
+                @Strike.canceled += instance.OnStrike;
+            }
+        }
+    }
+    public InGameActions @InGame => new InGameActions(this);
+    public interface IInGameActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnStrike(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float movementSpeed = 8f;
+    [SerializeField] private Animator _animator;
+    private Input _input;
+    private bool _flipped;
+
+    private void Awake()
+    {
+        _input = new Input();
+        _input.InGame.Jump.performed += context => Jump();
+        _input.InGame.Strike.performed += context => Strike();
+    }
+
+    private void OnEnable()
+    {
+        _input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _input.Disable();
+    }
+
+    private void Update()
+    {
+        Vector2 moveDirection = _input.InGame.Movement.ReadValue<Vector2>();
+        moveDirection *= movementSpeed * Time.deltaTime;
+        _animator.SetBool("moving", moveDirection.magnitude > 0.01);
+        transform.Translate(moveDirection);
+
+        checkFlip(moveDirection);
+
+
+    }
+
+    private void checkFlip(Vector2 moveDirection)
+    {
+        if (moveDirection.x != 0)
+        {
+            if (_flipped != moveDirection.x < 0)
+            {
+                _flipped = moveDirection.x < 0;
+                var transformLocalScale = transform.localScale;
+                transformLocalScale.x *= -1;
+                transform.localScale = transformLocalScale;
+            }
+        }
+    }
+
+    private void Jump()
+    {
+        _animator.SetTrigger("jump");
+    }
+
+    private void Strike()
+    {
+        int number = Random.Range(1, 3);
+        _animator.SetTrigger("strike" + number);
+        
+    }
+}
