bbc453d50a6b4df5f4831a815d0066fd42573287
zwarn <michael.budahn@gmail.com>
2021-01-03 16:50:48 +0100

    hitable barrels

---
+++ Assets/Scripts/HitableObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Collider2D))]
+public class HitableObject : MonoBehaviour
+{
+    [SerializeField] private Animator Animator;
+    [SerializeField] private int hits = 3;
+
+    public void Hit()
+    {
+        hits -= 1;
+        if (hits < 1)
+        {
+            Destroy();
+        }
+        else
+        {
+            Animator.SetTrigger("hit");
+        }
+    }
+    
+    
+    public void Destroy()
+    {
+        var collider = GetComponent<Collider2D>();
+        collider.enabled = false;
+        Animator.SetTrigger("destroy");
+        GetComponent<SpriteRenderer>().sortingLayerName = "LayingOnGround";
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,11 @@ using Random = UnityEngine.Random;
 
 public class PlayerController : MonoBehaviour
 {
-
     [SerializeField] private float movementSpeed = 8f;
-    [SerializeField] private Animator _animator;
+    [SerializeField] private Animator animator;
     private Input _input;
     private bool _flipped;
+    private bool _busy = false;
 
     private void Awake()
     {
@@ -29,17 +29,25 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        Vector2 moveDirection = _input.InGame.Movement.ReadValue<Vector2>();
-        moveDirection *= movementSpeed * Time.deltaTime;
-        _animator.SetBool("moving", moveDirection.magnitude > 0.01);
-        transform.Translate(moveDirection);
-
-        checkFlip(moveDirection);
-
-
+        handleMovement();
     }
 
-    private void checkFlip(Vector2 moveDirection)
+    private void handleMovement()
+    {
+        if (_busy)
+        {
+            return;
+        }
+
+        Vector2 moveDirection = _input.InGame.Movement.ReadValue<Vector2>();
+        moveDirection *= movementSpeed * Time.deltaTime;
+        animator.SetBool("moving", moveDirection.magnitude > 0.01);
+        transform.Translate(moveDirection);
+
+        handleFlip(moveDirection);
+    }
+
+    private void handleFlip(Vector2 moveDirection)
     {
         if (moveDirection.x != 0)
         {
@@ -55,13 +63,42 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        _animator.SetTrigger("jump");
+        if (!_busy)
+        {
+            animator.SetTrigger("jump");
+            _busy = true;
+        }
     }
 
     private void Strike()
     {
-        int number = Random.Range(1, 3);
-        _animator.SetTrigger("strike" + number);
-        
+        if (!_busy)
+        {
+            int number = Random.Range(1, 4);
+            animator.SetTrigger("strike" + number);
+            // animator.SetTrigger("strike1");
+            _busy = true;
+        }
+    }
+
+    private void Recover()
+    {
+        _busy = false;
+    }
+
+    private void StrikeHappens()
+    {
+        float radius = 0.5f;
+        float distance = 0.5f;
+        var colliderPosition = transform.position + new Vector3(_flipped ? -distance : distance, 0, 0);
+        var hitColliders = Physics2D.OverlapCircleAll(colliderPosition, radius);
+        foreach (var collider in hitColliders)
+        {
+            var hitableObject = collider.gameObject.GetComponent<HitableObject>();
+            if (hitableObject != null)
+            {
+                hitableObject.Hit();
+            }
+        }
     }
 }
